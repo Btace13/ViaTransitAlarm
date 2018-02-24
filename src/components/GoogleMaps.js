0f4acde2b5f45ba5f9052d911d967a5dcd31ad34
Roberto Navarro <rnavarro70@gmail.com>
2018-02-24 16:55:31 -0600

    add live data

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -3,75 +3,137 @@
 import React, { Component } from "react";
 import stop from ".././assets/stop.svg";
 import current from ".././assets/current.png";
+import axios from "axios";
 
 class Map extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            selectedStop: ''
+            selectedStop: "",
+            stops: []
+        };
     }
 
-    }
-    componentDidMount() {
-        let _this = this;
-        function initMap() {
-            let sa = { lat: 29.424065, lng: -98.48891 };
-            let map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 16,
-                center: sa
-            });
+    initMap() {
+        let sa = { lat: 29.424065, lng: -98.48891 };
+        let map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 16,
+            center: sa
+        });
 
-            // let temp = {
-            //     lat: sa.lat + 0.2,
-            //     lng: sa.lng + 0.2
-            // };
-            // let marker = new google.maps.Marker({
-            //     position: temp,
-            //     map: map,
-            //     icon: stop
-            // });
+        let stops = this.state.stops;
+        console.log("stops:", stops);
+        stops.forEach(e => {
+            console.log(e);
 
-            let markers = [
-                {
-                    stopId: "59886",
-                    position: { lat: 29.524509, lng: -98.392061 }
+            let marker = new google.maps.Marker({
+                position: {
+                    lat: e.Latitude,
+                    lng: e.Longitude
                 },
-                {
-                    stopId: "91139",
-                    position: { lat: 29.424065, lng: -98.48891 }
-                },
-                {
-                    stopId: "92399",
-                    position: { lat: 29.422739, lng: -98.483171 }
-                }
-            ];
-            markers.forEach(e => {
-                let marker = new google.maps.Marker({
-                    position: e.position,
-                    map: map,
-                    icon: stop,
-                    title: e.stopId
-                });
-
-                marker.addListener("click", () => {
-                    _this.setState({selectedStop: marker.getTitle()});
-                    _this.props.selectStop(_this.state.selectedStop);
-                });
-            });
-
-            let currentLocation = new google.maps.Marker({
-                position: sa,
-                height: 20,
-                width: 20,
                 map: map,
-                icon: {
-                    url: current,
-                    scaledSize: new google.maps.Size(70, 70) // scaled size
-                }
+                icon: stop,
+                title: e.stop_id
             });
-        }
 
-        initMap();
+            marker.addListener("click", () => {
+                this.setState({ selectedStop: marker.getTitle() });
+                this.props.selectStop(this.state.selectedStop);
+            });
+        });
+
+        let currentLocation = new google.maps.Marker({
+            position: sa,
+            height: 20,
+            width: 20,
+            map: map,
+            icon: {
+                url: current,
+                scaledSize: new google.maps.Size(70, 70) // scaled size
+            }
+        });
+    }
+
+    componentWillMount() {
+        axios
+            .get(
+                "https://joelgilbert.io/buses/closestops?routeid=17&longitude=-98.48891&latitude=29.424065&isOutbound=false&radius=1600"
+            )
+            .then(response => {
+                this.setState({
+                    stops: response.data
+                });
+                console.log(JSON.stringify(this.state.stops, null, 4));
+
+                this.initMap();
+            });
+    }
+
+    componentDidMount() {
+        // let _this = this;
+        // function initMap() {
+        //     let sa = { lat: 29.424065, lng: -98.48891 };
+        //     let map = new google.maps.Map(document.getElementById("map"), {
+        //         zoom: 16,
+        //         center: sa
+        //     });
+        //     // let temp = {
+        //     //     lat: sa.lat + 0.2,
+        //     //     lng: sa.lng + 0.2
+        //     // };
+        //     // let marker = new google.maps.Marker({
+        //     //     position: temp,
+        //     //     map: map,
+        //     //     icon: stop
+        //     // });
+        //     // let markers = [
+        //     //     {
+        //     //         stopId: "59886",
+        //     //         position: { lat: 29.524509, lng: -98.392061 }
+        //     //     },
+        //     //     {
+        //     //         stopId: "91139",
+        //     //         position: { lat: 29.424065, lng: -98.48891 }
+        //     //     },
+        //     //     {
+        //     //         stopId: "92399",
+        //     //         position: { lat: 29.422739, lng: -98.483171 }
+        //     //     }
+        //     // ];
+        //     let stops = _this.state.stops;
+        //     console.log("stops:", stops);
+        //     stops.forEach(e => {
+        //         // let position = {
+        //         //     lat: e.Latitude,
+        //         //     lon: e.Longitude
+        //         // };
+        //         console.log(e);
+        //         let marker = new google.maps.Marker({
+        //             position: {
+        //                 lat: e.Latitude,
+        //                 lng: e.Longitude
+        //             },
+        //             map: map,
+        //             icon: stop,
+        //             title: e.stop_id
+        //         });
+        //         marker.addListener("click", () => {
+        //             _this.setState({ selectedStop: marker.getTitle() });
+        //             _this.props.selectStop(_this.state.selectedStop);
+        //         });
+        //     });
+        //     let currentLocation = new google.maps.Marker({
+        //         position: sa,
+        //         height: 20,
+        //         width: 20,
+        //         map: map,
+        //         icon: {
+        //             url: current,
+        //             scaledSize: new google.maps.Size(70, 70) // scaled size
+        //         }
+        //     });
+        // }
+        // initMap();
     }
 
     render() {
@@ -98,7 +160,10 @@ class Map extends Component {
             <div>
                 <div style={styles} id="map" />
                 <div className="container">
-                    <p className={'flow-text left'} style={{marginBottom: 0, paddingBottom:0}}>{`BUS STOP: ${this.state.selectedStop}`}</p>
+                    <p
+                        className={"flow-text left"}
+                        style={{ marginBottom: 0, paddingBottom: 0 }}
+                    >{`BUS STOP: ${this.state.selectedStop}`}</p>
                 </div>
             </div>
         );
