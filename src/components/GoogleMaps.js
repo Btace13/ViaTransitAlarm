6a1e4447ac3833ace06d978e40994eec4fe89347
Roberto Navarro <rnavarro70@gmail.com>
2018-02-24 20:45:59 -0600

    fix issue

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -3,48 +3,51 @@
 import React, { Component } from "react";
 import stop from ".././assets/stop.svg";
 import axios from "axios";
-import { ToastContainer, toast } from 'react-toastify';
-import Form from '../components/Form';
+import { ToastContainer, toast } from "react-toastify";
+import Form from "../components/Form";
 import current from ".././assets/current.png";
 
 class Map extends Component {
+    map = {};
+    INBOUND_API_URL = "https://joelgilbert.io/buses/closestops?routeid=17&longitude=-98.48891&latitude=29.424065&isOutbound=false&radius=1600";
+    OUTBOUND_API_URL = "https://joelgilbert.io/buses/closestops?routeid=17&longitude=-98.48891&latitude=29.424065&isOutbound=true&radius=1600";
+
     constructor() {
         super();
         this.state = {
-            direction: 'Inbound',
+            direction: "Inbound",
             departureTimes: [],
-            selectedDepatureTime: '',
+            selectedDepatureTime: "",
             notiftyTime: 0,
-            selectedTimeInc: '',
+            selectedTimeInc: "",
             textOrCall: true,
             phoneNumber: 0,
             isDisabled: true
-        }
+        };
     }
 
     initMap() {
         let sa = { lat: 29.424065, lng: -98.48891 };
-        let map = new google.maps.Map(document.getElementById("map"), {
+        this.map = new google.maps.Map(document.getElementById("map"), {
             zoom: 16,
             center: sa
         });
 
         let stops = this.state.stops;
         stops.forEach(e => {
-
             let marker = new google.maps.Marker({
                 position: {
                     lat: e.Latitude,
                     lng: e.Longitude
                 },
-                map: map,
+                map: this.map,
                 icon: stop,
                 title: e.stop_id
             });
 
             marker.addListener("click", () => {
                 this.setState({ selectedStop: marker.getTitle() });
-                toast.warn('BUS STOP: ' + marker.getTitle());
+                toast.warn("BUS STOP: " + marker.getTitle());
                 this.enableForm();
             });
         });
@@ -52,7 +55,7 @@ class Map extends Component {
             position: sa,
             height: 20,
             width: 20,
-            map: map,
+            map: this.map,
             icon: {
                 url: current,
                 scaledSize: new google.maps.Size(70, 70) // scaled size
@@ -61,34 +64,37 @@ class Map extends Component {
     }
 
     componentWillMount() {
-            this.state.direction === 'Inbound' ? this.getInBound(): this.getOutBound()
+        this.state.direction === "Inbound"
+            ? this.getInBound()
+            : this.getOutBound();
+        // this.state.direction === "Outbound"
+        //     ? this.getInBound()
+        //     : this.getOutBound();
     }
 
-
-    INBOUND_API_URL = "https://joelgilbert.io/buses/closestops?routeid=17&longitude=-98.48891&latitude=29.424065&isOutbound=false&radius=1600";
-    OUTBOUND_API_URL = "https://joelgilbert.io/buses/closestops?routeid=17&longitude=-98.48891&latitude=29.424065&isOutbound=true&radius=1600";
     getOutBound = () => {
-        axios.get(this.OUTBOUND_API_URL)
-            .then(response => {
-                this.setState({
-                    stops: response.data
-                });
-
-                this.initMap();
+        axios.get(this.OUTBOUND_API_URL).then(response => {
+            this.setState({
+                stops: response.data
             });
+
+            // console.log("this.map:", this.map);
+            console.log("inside getOutbound()");
+
+            this.initMap();
+        });
     };
     getInBound = () => {
-        axios.get(this.INBOUND_API_URL)
-            .then(response => {
-                this.setState({
-                    stops: response.data
-                });
-
-                this.initMap();
+        axios.get(this.INBOUND_API_URL).then(response => {
+            this.setState({
+                stops: response.data
             });
+            console.log("inside getInBound()");
+            this.initMap();
+        });
     };
 
-    updateParent = (value) => {
+    updateParent = value => {
         this.setState({
             Inbound: value.Inbound,
             selectedDepatureTime: value.selectedDepatureTime,
@@ -96,21 +102,23 @@ class Map extends Component {
             selectedTimeInc: value.selectedTimeInc,
             textOrCall: value.textOrCall,
             phoneNumber: value.phoneNumber
-
         });
     };
 
     updateMap = () => {
-        this.state.direction === 'Inbound' ? this.getInBound(): this.getOutBound()
+        // console.log("Inside updateMap");
+        this.state.direction === "Inbound"
+            ? this.getOutBound()
+            : this.getInBound();
     };
 
     enableForm = () => {
         this.setState({
             isDisabled: false
-        })
+        });
     };
 
-    changeDirection = (direction) => {
+    changeDirection = direction => {
         this.setState({
             direction: direction
         });
@@ -120,11 +128,13 @@ class Map extends Component {
         this.updateMap();
     };
 
-    handleChangeDirection = (event) => {
-        console.log(event.target.id);
+    handleChangeDirection = (e, data) => {
+        // console.log(e.target.text);
+
         this.setState({
-            direction: event.target.id
+            direction: e.target.text
         });
+
         this.updateMap();
     };
     render() {
@@ -151,12 +161,33 @@ class Map extends Component {
             <div>
                 <div style={styles} id="map" />
                 <div className="container">
-                    <ul className="tabs" style={{marginBottom: '30px'}}>
-                        <li className="tab"><a onClick={this.handleChangeDirection} id={'Inbound'} className="active">Inbound</a></li>
-                        <li className="tab"><a onClick={this.handleChangeDirection} id={'Outbound'} >Outbound</a></li>
+                    <ul className="tabs" style={{ marginBottom: "30px" }}>
+                        <li className="tab">
+                            <a
+                                onClick={this.handleChangeDirection}
+                                id={"Inbound"}
+                                value="Pizza"
+                                className="active"
+                            >
+                                Inbound
+                            </a>
+                        </li>
+                        <li className="tab">
+                            <a
+                                onClick={this.handleChangeDirection}
+                                id={"Outbound"}
+                                value="Pizza"
+                            >
+                                Outbound
+                            </a>
+                        </li>
                     </ul>
                 </div>
-                <Form  changeDirection={this.changeDirection} isDisabled={this.state.isDisabled} updateParent={this.updateParent}/>
+                <Form
+                    changeDirection={this.changeDirection}
+                    isDisabled={this.state.isDisabled}
+                    updateParent={this.updateParent}
+                />
                 <ToastContainer />
             </div>
         );
